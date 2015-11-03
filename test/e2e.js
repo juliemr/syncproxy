e6d5a9f5dea6c6f6ed599f2c2cf3b2c5fc688843
Julie Ralph <ju.ralph@gmail.com>
2015-11-02 16:59:18 -0800

    add package.json and tests

---
+++ test/e2e.js
@@ -0,0 +1,41 @@
+var webdriver = require('selenium-webdriver');
+
+// Assumes that:
+// - a stability proxy is running at port 8111
+// - a selenium standalone is running at port 4444
+// - the test application is running at port 8081
+
+var driver = new webdriver.Builder().
+  usingServer('http://localhost:8111').
+  withCapabilities(webdriver.Capabilities.chrome()).
+  build();
+
+describe('stability proxy', function() {
+  afterAll(function(done) {
+    driver.quit().then(done, done.fail);
+  });
+
+  it('should get a page with Angular and wait for a slow action', function(done) {
+    driver.manage().timeouts().setScriptTimeout(20000);
+    driver.get('http://localhost:8081/index.html#/async');
+    driver.findElement(webdriver.By.css('[ng-bind="slowHttpStatus"]')).getText().then(function(text) {
+      expect(text).toEqual('not started');
+    });
+    driver.findElement(webdriver.By.css('[ng-click="slowHttp()"]')).click();
+    driver.findElement(webdriver.By.css('[ng-bind="slowHttpStatus"]')).getText().then(function(text) {
+      expect(text).toEqual('done');
+      done();
+    }).thenCatch(done.fail);
+  }, 10000);
+
+  it('should fail when angular is not available', function(done) {
+    driver.manage().timeouts().setScriptTimeout(20000);
+    driver.get('about:blank');
+    driver.executeScript('var x = 20').then(function() {
+      done.fail('expected driver.execute to fail, but it did not'); 
+    }, function(err) {
+      expect(err).toMatch('angular could not be found on the window');
+      done();
+    });
+  }, 10000);
+});
